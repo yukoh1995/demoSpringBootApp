8130a6f2130ab8f6c26ed51251059a5fa65dbfe5
yugakida <44520878+yugakida@users.noreply.github.com>
2021-01-11 15:47:37 +0900

    first commit

---
+++ src/test/java/com/kidayuga/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kidayuga.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
