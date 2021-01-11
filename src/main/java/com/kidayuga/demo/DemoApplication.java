0b58ec8ed1e91d3fb88c703afc7e7e420d6b031f
木田湧雅 <kidayuga19950623@gmail.com>
2021-01-11 15:56:28 +0900

    ビックリマーク追加

--- src/main/java/com/kidayuga/demo/DemoApplication.java
+++ src/main/java/com/kidayuga/demo/DemoApplication.java
@@ -11,7 +11,7 @@ public class DemoApplication {
 
     @RequestMapping("/")
     public String hello() {
-        return "Hello World!!";
+        return "Hello World!!!";
     }
 
     public static void main(String[] args) {
